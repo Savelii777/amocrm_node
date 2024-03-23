0e5477268817511dbcb66cc97eb48eec769c6775
Savelii777 <sporyshev.savelii@gmail.com>
2024-03-23 03:39:49 +0300

    данные пользователя63

--- index.js
+++ index.js
@@ -289,7 +289,7 @@ connection.connect((err) => {
             statuses.then((response) => {
                 console.log(JSON.stringify(response.data.status_id, null, 2)); // Красивый вывод всего объекта с отступам
                 console.log(results[0].booking_status_id)
-                const query1 = 'UPDATE bookings SET booking_status_id = 2 WHERE client_id = 993';
+                const query1 = 'UPDATE bookings SET booking_status_id = 3 WHERE client_id = 993';
                 if (results[0].booking_status_id == 1 && bookingStatus.NEW != response.data.status_id) {
                     connection.query(query1, [2, results[0].id], (err, result) => {
                         if (err) {
